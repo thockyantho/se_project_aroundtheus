7c10c4fa3b42398cbb1c54e37a7170768b1036ef
thockyantho <anthopham94@gmail.com>
2023-06-26 14:18:55 -0700

    1st update for review

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,7 @@ const cardTemplate =
 /*                  Functions                    */
 /*-----------------------------------------------*/
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -55,11 +55,10 @@ function getCardElement(cardData) {
   // access the card title and image and store them in variables
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
-  const cardImgAltText = cardElement.querySelector(".card__description");
   // set the path to the image to the link field of the object
   cardImageEl.src = cardData.link;
   // set the image alt text to the name field of the object
-  cardImgAltText.alt = cardData.name;
+  cardImageEl.alt = cardData.name;
   // set the card title to the name field of the object, too
   cardTitleEl.textContent = cardData.name;
   // return the ready HTML element with the filled-in data
@@ -81,7 +80,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileCloseButton.addEventListener("click", closePopup);
