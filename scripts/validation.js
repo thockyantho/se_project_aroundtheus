d85c5858d4f4546d86abe46cd6dee10bb18b7d36
thockyantho <anthopham94@gmail.com>
2023-08-07 14:26:42 -0700

    deleted comment in valid.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -70,16 +70,6 @@ function enableValidation(options) {
     });
 
     setEventListeners(formEl, options);
-    // look for all inputs inside of form
-    // loop through all the inputs to see if all are valid
-    // if input is not valid
-    // get validation message
-    // add error class to input
-    // display error message
-    // disable button
-    // if all inputs are valid
-    // enable button
-    // reset error messages
   });
 }
 
