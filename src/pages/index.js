0fac30cf0343527e25c7df21003420789697f7a0
thockyantho <anthopham94@gmail.com>
2023-09-26 19:17:24 -0700

    added listeners and functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,89 @@ import "../pages/index.css";
 // Import all the classes
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
-import { closeModal, openModal } from "../utils/utils.js";
 import Section from "../components/Section.js";
 import { config, initialCards, selectors } from "../utils/constants.js";
 import PopupWithImage from "../components/PopupWithImage";
+import PopupWithForm from "../components/PopupWithForm";
+import UserInfo from "../components/UserInfo";
+
+/*---------------------------------------------------------------------------*/
+/*                               Element                                     */
+/*---------------------------------------------------------------------------*/
+
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditButton = document.querySelector("#profile-edit-button");
+const addCardModal = document.querySelector("#add-card-modal");
+const addNewCardButton = document.querySelector(".profile__add-button");
+
+const profileNameInput = document.querySelector("#profile-name-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const addCardFormElement = addCardModal.querySelector(".modal__form");
 
 // Create instances of the classes
+
+const editFormValidator = new FormValidator(config, addCardFormElement);
+const cardFormValidator = new FormValidator(config, profileEditForm);
+
+function InputProfileForm() {
+  const userData = userInfo.getUserInfo();
+  profileNameInput.value = userData.name;
+  profileDescriptionInput.value = userData.description;
+}
+
+/*---------------------------------------------------------------------------*/
+/*                               Event Handlers                              */
+/*---------------------------------------------------------------------------*/
+const userInfo = new UserInfo(".profile__title", ".profile__description");
+
+function handleProfileEditSubmit(formData) {
+  userInfo.setUserInfo(formData.name, formData.description);
+  formProfileEditModal.close();
+}
+
+/*---------------------------------------------------------------------------*/
+/*                               Event Listeners                             */
+/*---------------------------------------------------------------------------*/
+
+const handleEditClick = () => {
+  InputProfileForm();
+  formProfileEditModal.open();
+};
+
+profileEditButton.addEventListener("click", handleEditClick);
+
+function handleAddCardFormSubmit({ title, description }) {
+  const cardData = {
+    name: title,
+    link: description,
+  };
+
+  const card = renderCard(cardData);
+  cardSection.addItem(card);
+  addCardPopup.close();
+}
+
+addNewCardButton.addEventListener("click", () => {
+  addCardPopup.open();
+  cardFormValidator.toggleButtonState();
+});
+
+function renderCard(cardData) {
+  const card = new Card(cardData, selectors.cardTemplate, (imgData) => {
+    cardPreviewImageModal.open(imgData);
+  });
+
+  return card.getView();
+}
+
 const CardPreviewPopup = new PopupWithImage(selectors.previewPopup);
 
 const CardSection = new Section(
   {
-    // items: initialCards,
+    items: initialCards,
     renderer: (data) => {
       const cardEl = new Card(
         {
@@ -30,12 +102,24 @@ const CardSection = new Section(
   selectors.cardSection
 );
 
-const editFormValidator = new FormValidator(config, editFormModalWindow);
-const cardFormValidator = new FormValidator(config, cardFormModalWindow);
 // Initiallize all my instances
 CardSection.renderItems(initialCards);
 CardPreviewPopup.setEventListeners();
 editFormValidator.enableValidation();
 cardFormValidator.enableValidation();
 
+const addCardPopup = new PopupWithForm(
+  "#add-card-modal",
+  handleAddCardFormSubmit
+);
+addCardPopup.setEventListeners();
+
 // All the rest
+const formProfileEditModal = new PopupWithForm(
+  selectors.profileEditModal,
+  handleProfileEditSubmit
+);
+formProfileEditModal.setEventListeners();
+
+export { CardPreviewPopup };
+export { formProfileEditModal };
