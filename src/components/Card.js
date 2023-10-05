89de431af8c91d9143b3b85b6a558c449f2fd088
thockyantho <anthopham94@gmail.com>
2023-10-05 00:33:55 -0700

    fixed: cleaned up Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,30 +4,32 @@ export default class Card {
     this._link = link;
     this._cardSelector = cardSelector;
     this._handlePreviewImage = handlePreviewImage;
+
+    // frequently accessed elements
+    this._cardElement = this._getTemplate();
+    this._cardImage = this._cardElement.querySelector(".card__image");
+    this._likeButton = this._cardElement.querySelector(".card__like-button");
+    this._trashButton = this._cardElement.querySelector(".card__trash-button");
+
+    this._setEventListeners();
   }
 
   _setEventListeners() {
-    this._cardElement
-      .querySelector(".card__image")
-      .addEventListener("click", () => {
-        this._handlePreviewImage(this._name, this._link);
-      });
+    this._cardImage.addEventListener("click", () => {
+      this._handlePreviewImage(this._name, this._link);
+    });
     // select like button
-    this._likeButton = this._cardElement.querySelector(".card__like-button");
     this._likeButton.addEventListener("click", () => {
       this._handleLikeIcon();
     });
     // select delete
-    this._trashButton = this._cardElement.querySelector(".card__trash-button");
     this._trashButton.addEventListener("click", () => {
       this._handleTrashButton();
     });
   }
 
   _handleLikeIcon() {
-    this._cardElement
-      .querySelector(".card__like-button")
-      .classList.toggle("card__like-button_active");
+    this._likeButton.classList.toggle("card__like-button_active");
   }
 
   _handleTrashButton() {
@@ -43,24 +45,10 @@ export default class Card {
   }
 
   getView() {
-    this._cardElement = this._getTemplate();
-
-    // // select like button
-    // this._likeButton = this._cardElement.querySelector(".card__like-button");
-    // this._likeButton.addEventListener("click", () => {
-    //   this._handleLikeIcon();
-    // });
-    // // select delete
-    // this._trashButton = this._cardElement.querySelector(".card__trash-button");
-    // this._trashButton.addEventListener("click", () => {
-    //   this._handleTrashButton();
-    // });
-
-    this._cardElement.querySelector(".card__image").src = this._link;
-    this._cardElement.querySelector(".card__image").alt = this._name;
+    this._cardImage.src = this._link;
+    this._cardImage.alt = this._name;
     this._cardElement.querySelector(".card__title").textContent = this._name;
 
-    this._setEventListeners();
     return this._cardElement;
   }
 }
